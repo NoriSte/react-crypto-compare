4d94917135c4763cbb0d22780b800ec6e23a9e79
NoriSte <nori.ste.magni@gmail.com>
2019-05-30 10:08:11 +0200

    style: 💄 Enable Prettier

--- cypress/integration/react.spec.js
+++ cypress/integration/react.spec.js
@@ -1,19 +1,27 @@
-
 /// <reference types="Cypress" />
 
 // import the component you want to test
-import React from 'react';
-import CryptoCompare, { amountClassName, currencyClassName, defaultClassName, emptyResult, errorClassName, getApikeyAuthorizationHeader, getApiUrl, loadingClassName, setApikey } from '../../src/index.tsx';
-import fixReactDomScope from '../support/fixReactDomScope';
+import React from "react";
+import CryptoCompare, {
+  amountClassName,
+  currencyClassName,
+  defaultClassName,
+  emptyResult,
+  errorClassName,
+  getApikeyAuthorizationHeader,
+  getApiUrl,
+  loadingClassName,
+  setApikey
+} from "../../src/index.tsx";
+import fixReactDomScope from "../support/fixReactDomScope";
 
-
-describe('CryptoCompare component', () => {
+describe("CryptoCompare component", () => {
   const amount = 10;
   const from = "EUR";
   const to = "BTC";
   const apikey = "9dbe3a144881f49682bf50733db69eb57b2aaa07f6e71f1fb03fb3dbea257695";
   const url = "**/data/price**";
-  const response = {[to]: 0.0002 };
+  const response = { [to]: 0.0002 };
 
   const checkComponent = (amount, currency, selector = `.${defaultClassName}`) => {
     cy.contains(amount);
@@ -21,13 +29,16 @@ describe('CryptoCompare component', () => {
     cy.get(selector).should("be.visible");
     cy.get(`.${amountClassName}`).should("be.visible");
     cy.get(`.${currencyClassName}`).should("be.visible");
-  }
+  };
   const waitAndcheckApikey = (alias = "@cryptocompare", key = apikey) => {
     cy.wait(alias).then(xhr => {
-      expect(xhr.url).to.eq(getApiUrl(from, to))
-      expect(xhr.request.headers).to.have.property("Authorization", getApikeyAuthorizationHeader(key))
-    })
-  }
+      expect(xhr.url).to.eq(getApiUrl(from, to));
+      expect(xhr.request.headers).to.have.property(
+        "Authorization",
+        getApikeyAuthorizationHeader(key)
+      );
+    });
+  };
 
   beforeEach(function() {
     // see https://github.com/bahmutov/cypress-react-unit-test/issues/51
@@ -35,59 +46,63 @@ describe('CryptoCompare component', () => {
     fixReactDomScope(window);
   });
 
-  describe('Integration tests', () => {
-    it('Should convert the currency', function() {
+  describe("Integration tests", () => {
+    it("Should convert the currency", function() {
       cy.route(url, response).as("cryptocompare");
-      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />)
+      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />);
       cy.get(`.${loadingClassName}`).should("be.visible");
       waitAndcheckApikey();
       checkComponent(amount * response[to], to);
-    })
+    });
 
-    it('Should manage a service response error', function() {
+    it("Should manage a service response error", function() {
       const response = { Message: "fsym param is empty or null." };
       cy.route(url, response).as("cryptocompare");
-      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />)
+      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />);
       waitAndcheckApikey();
       checkComponent(emptyResult, to, `.${errorClassName}`);
-    })
+    });
 
-    it('Should manage silently a network error', function() {
-      cy.route({url, response: "", status: 400}).as("cryptocompare");
-      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />)
+    it("Should manage silently a network error", function() {
+      cy.route({ url, response: "", status: 400 }).as("cryptocompare");
+      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />);
       waitAndcheckApikey();
       checkComponent(emptyResult, to, `.${errorClassName}`);
-    })
+    });
 
-    it('Should allow to set the api key in advance', function() {
+    it("Should allow to set the api key in advance", function() {
       cy.route(url, response).as("cryptocompare");
       setApikey(apikey);
-      cy.mount(<CryptoCompare from={from} to={to} amount={amount} />)
+      cy.mount(<CryptoCompare from={from} to={to} amount={amount} />);
       waitAndcheckApikey();
       checkComponent(amount * response[to], to);
-    })
+    });
 
-    it('Should give precedence to the passed api key', function() {
+    it("Should give precedence to the passed api key", function() {
       cy.route(url, response).as("cryptocompare");
       setApikey("global-apikey");
-      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />)
+      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />);
       waitAndcheckApikey();
       checkComponent(amount * response[to], to);
-    })
+    });
 
-    it('Should manage multiple currencies selecting the first one', function() {
+    it("Should manage multiple currencies selecting the first one", function() {
       cy.route(url, response).as("cryptocompare");
-      cy.mount(<CryptoCompare from={`${from},USD`} to={`${to},BCH`} amount={amount} apikey={apikey} />)
+      cy.mount(
+        <CryptoCompare from={`${from},USD`} to={`${to},BCH`} amount={amount} apikey={apikey} />
+      );
       waitAndcheckApikey();
       checkComponent(amount * response[to], to);
-    })
-  })
+    });
+  });
 
-  describe('E2E tests', () => {
-    it('Should convert the currency', function() {
+  describe("E2E tests", () => {
+    it("Should convert the currency", function() {
       cy.route(url).as("cryptocompare");
-      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />)
-      cy.wait("@cryptocompare").its(`response.body.${to}`).then(conversion => checkComponent(amount * conversion, to, `.${defaultClassName}`));
-    })
-  })
-})
+      cy.mount(<CryptoCompare from={from} to={to} amount={amount} apikey={apikey} />);
+      cy.wait("@cryptocompare")
+        .its(`response.body.${to}`)
+        .then(conversion => checkComponent(amount * conversion, to, `.${defaultClassName}`));
+    });
+  });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,51 @@
-import axios, { AxiosResponse } from 'axios';
-import * as PropTypes from 'prop-types';
-import * as React from 'react';
-import { is } from 'typescript-is-type';
-
+import axios, { AxiosResponse } from "axios";
+import * as PropTypes from "prop-types";
+import * as React from "react";
+import { is } from "typescript-is-type";
 
 export type CryptoCompareValues = {
-  [key: string]: number
-}
+  [key: string]: number;
+};
 export type CryptoCompareError = {
-  Message: string
-}
-export type CryptoCompareResponse = CryptoCompareValues|CryptoCompareError;
+  Message: string;
+};
+export type CryptoCompareResponse = CryptoCompareValues | CryptoCompareError;
 
 /**
-* @typedef {Object} Props
-* @property {string} apikey - the cryptocompare.com api key
-* @property {string} from - the origin cryptocurrency/currency symbol of interest
-* @property {string} to - the destination cryptocurrency/currency symbol of interest
-* @property {number} amount - the amount to be converted
-*/
+ * @typedef {Object} Props
+ * @property {string} apikey - the cryptocompare.com api key
+ * @property {string} from - the origin cryptocurrency/currency symbol of interest
+ * @property {string} to - the destination cryptocurrency/currency symbol of interest
+ * @property {number} amount - the amount to be converted
+ */
 export type Props = {
-  apikey?: string,
-  from: string,
-  to: string,
-  amount: number,
-}
+  apikey?: string;
+  from: string;
+  to: string;
+  amount: number;
+};
 
-let globalApikey:string = '';
+let globalApikey: string = "";
 
 /**
-* Set the API key for every future call
-* @param {string} apikey
-*/
-export const setApikey = (apikey:string) => globalApikey = apikey;
+ * Set the API key for every future call
+ * @param {string} apikey
+ */
+export const setApikey = (apikey: string) => (globalApikey = apikey);
 /**
-* Get the authorization header for the cryptocompare.com API
-* @param {string} apikey
-* @return {string}
-*/
-export const getApikeyAuthorizationHeader = (apikey:string) => `Apikey ${apikey}`;
+ * Get the authorization header for the cryptocompare.com API
+ * @param {string} apikey
+ * @return {string}
+ */
+export const getApikeyAuthorizationHeader = (apikey: string) => `Apikey ${apikey}`;
 /**
-* Get the cryptocompare.com API URL
-* @param {string} from
-* @param {string} to
-* @return {string}
-*/
-export const getApiUrl = (from:string, to:string) => `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`;
-
+ * Get the cryptocompare.com API URL
+ * @param {string} from
+ * @param {string} to
+ * @return {string}
+ */
+export const getApiUrl = (from: string, to: string) =>
+  `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`;
 
 export const emptyResult = "---";
 export const defaultClassName = "react-crypto-compare";
@@ -56,47 +55,41 @@ export const amountClassName = "react-crypto-compare-amount";
 export const currencyClassName = "react-crypto-compare-currency";
 
 /**
-* @param props
-* @param props.apikey the cryptocompare.com api key
-* @param props.from The email of the user.
-* @param props.to The email of the user.
-* @param props.amount The email of the user.
-*/
-const CryptoCompare: React.FunctionComponent<Props> = ({
-  apikey,
-  from,
-  to,
-  amount
-}:Props) => {
+ * @param props
+ * @param props.apikey the cryptocompare.com api key
+ * @param props.from The email of the user.
+ * @param props.to The email of the user.
+ * @param props.amount The email of the user.
+ */
+const CryptoCompare: React.FunctionComponent<Props> = ({ apikey, from, to, amount }: Props) => {
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<string|undefined>(undefined);
-  const [data, setData] = React.useState<CryptoCompareValues|undefined>(undefined);
+  const [error, setError] = React.useState<string | undefined>(undefined);
+  const [data, setData] = React.useState<CryptoCompareValues | undefined>(undefined);
 
-  if(!apikey && !globalApikey) {
+  if (!apikey && !globalApikey) {
     throw new Error("'apikey' (or a global apikey set with 'setApikey') is required");
   }
-  if(!from) {
+  if (!from) {
     throw new Error("'from' is required");
   }
-  if(!to) {
+  if (!to) {
     throw new Error("'to' is required");
   }
-  if(isNaN(amount)) {
+  if (isNaN(amount)) {
     throw new Error("'amount' must be a number");
   }
 
-  if(from.includes(",")) {
+  if (from.includes(",")) {
     console.info("Multiple currencies aren't supported yet");
     from = from.split(",")[0];
   }
-  if(to.includes(",")) {
+  if (to.includes(",")) {
     console.info("Multiple currencies aren't supported yet");
     to = to.split(",")[0];
   }
 
   React.useEffect(() => {
     const fetchData = async () => {
-
       const headers = {
         Authorization: getApikeyAuthorizationHeader(apikey || globalApikey)
       };
@@ -104,27 +97,27 @@ const CryptoCompare: React.FunctionComponent<Props> = ({
       setLoading(true);
       let response: AxiosResponse<CryptoCompareResponse>;
       try {
-        response = await axios.get(getApiUrl(from, to), {headers});
-      } catch(e){
+        response = await axios.get(getApiUrl(from, to), { headers });
+      } catch (e) {
         response = e.response;
       }
 
-      if(response && response.data && is<CryptoCompareError>(response.data, "Message")) {
+      if (response && response.data && is<CryptoCompareError>(response.data, "Message")) {
         setError((response.data as CryptoCompareError).Message);
       } else {
         setError("Error");
       }
 
       setLoading(false);
-      if(response && response.data && is<CryptoCompareValues>(response.data, to)) {
-        setData(response.data)
+      if (response && response.data && is<CryptoCompareValues>(response.data, to)) {
+        setData(response.data);
       }
     };
     fetchData();
   }, []);
 
   React.useEffect(() => {
-    if(error) {
+    if (error) {
       console.log("react-crypto-compare", error);
     }
   }, [error]);
@@ -132,18 +125,22 @@ const CryptoCompare: React.FunctionComponent<Props> = ({
   const printResult = !!data && is<CryptoCompareValues>(data, to);
 
   return (
-    <div className={`${defaultClassName} ${error ? errorClassName : ''} ${loading ? loadingClassName : ''}`}>
+    <div
+      className={`${defaultClassName} ${error ? errorClassName : ""} ${
+        loading ? loadingClassName : ""
+      }`}
+    >
       <span className={amountClassName}>
         {printResult ? is<CryptoCompareValues>(data, to) && data[to] * amount : emptyResult}
       </span>{" "}
       <span className={currencyClassName}>{to}</span>
     </div>
-  )
-}
+  );
+};
 
 CryptoCompare.propTypes = {
   apikey: (props: Props, propName: string, componentName: string) => {
-    let error: Error|null = null;
+    let error: Error | null = null;
     const prop = props[propName];
     if (prop && typeof prop !== "string") {
       error = new Error(`${componentName} - ${propName} must be a string`);
@@ -151,13 +148,13 @@ CryptoCompare.propTypes = {
     if (!prop && !globalApikey) {
       error = new Error(
         `${componentName} - ${propName} must be defined, set the component prop or use the 'setApikey' function`
-        );
-      }
-      return error;
-    },
-    from: PropTypes.string.isRequired,
-    to: PropTypes.string.isRequired,
-    amount: PropTypes.number.isRequired
-  };
+      );
+    }
+    return error;
+  },
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired
+};
 
-  export default CryptoCompare;
+export default CryptoCompare;
